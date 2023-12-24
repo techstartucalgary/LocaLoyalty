d038e3261bb5cd8b06a6d69d3aed7b7f357e46e2
Bradley Li <jizhe.li@ucalgary.ca>
2023-12-23 22:23:04 -0700

    1. Set up both sign-in and sign-up clerk authentication pages; 2.set up the authentication middleware which default directs the ./ page'

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
-import Image from "next/image";
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import { trpc } from "@/lib/trpc";
-import Array from "@/components/Array";
+import Head from "next/head";
+import Link from "next/link";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   const [queryClient] = useState(() => new QueryClient());
@@ -21,47 +22,25 @@ export default function Home() {
   return (
     <trpc.Provider queryClient={queryClient} client={trpcClient}>
       <QueryClientProvider client={queryClient}>
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-            <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-              Get started by editing&nbsp;
-              <code className="font-mono font-bold">src/app/page.tsx</code>
-            </p>
-            <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-              <a
-                className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                By{" "}
-                <Image
-                  src="/vercel.svg"
-                  alt="Vercel Logo"
-                  className="dark:invert"
-                  width={100}
-                  height={24}
-                  priority
-                />
-              </a>
-            </div>
-          </div>
-
-          <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-            <Image
-              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-              src="/next.svg"
-              alt="Next.js Logo"
-              width={180}
-              height={37}
-              priority
+        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-500 to-purple-500">
+          <Head>
+            <title>Hello Pages Router with Next.js & Clerk</title>
+            <meta
+              name="description"
+              content="A simple Hello World homepage using Next.js and Clerk"
             />
-          </div>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
 
-          <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-            <Array />
+          <div className="bg-white p-8 rounded-xl shadow-lg">
+            <h1 className="text-2xl font-bold mb-4 text-blue-500">
+              Hello, Pages Router!
+            </h1>
+            <p className="text-gray-600">
+              This is a simple homepage built with Next.js and Clerk
+            </p>
           </div>
-        </main>
+        </div>
       </QueryClientProvider>
     </trpc.Provider>
   );
