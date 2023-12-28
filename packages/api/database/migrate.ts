25af2b48429c463c74d89b3c8ee4753ff1f8a86d
mpagels3022 <101290617+mpagels3022@users.noreply.github.com>
2023-12-28 15:42:13 -0700

    Created migrate.ts

---
+++ packages/api/database/migrate.ts
@@ -0,0 +1,32 @@
+/*
+Run this code with node to migrate the schema to Planetscale
+*/
+
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { connect } from "@planetscale/database";
+import { migrate } from "drizzle-orm/planetscale-serverless/migrator";
+import * as schema from "./schema";
+
+// create the connection
+const connection = connect({
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+});
+
+// export connection for use in other files
+export const db = drizzle(connection, { schema });
+
+async function main() {
+  console.log("Migration started...");
+  await migrate(db, { migrationsFolder: "drizzle-migrations" });
+  console.log("Migration ended.");
+  process.exit(0);
+}
+
+main().catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
+
---
+++ packages/api/database/schema.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.users = void 0;
+var mysql_core_1 = require("drizzle-orm/mysql-core");
+exports.users = (0, mysql_core_1.mysqlTable)("users", {
+    id: (0, mysql_core_1.serial)("id").primaryKey(),
+    fullName: (0, mysql_core_1.text)("full_name"),
+    phone: (0, mysql_core_1.varchar)("phone", { length: 256 }),
+    address: (0, mysql_core_1.varchar)("address", { length: 256 }),
+});
+//TODO...
