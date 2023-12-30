11e26dc2202624b41afae67f6d3c7f0585f543a7
mpagels3022 <101290617+mpagels3022@users.noreply.github.com>
2023-12-29 18:44:23 -0700

    added constraints and foreign keys to db schema

--- packages/api/database/schema.ts
+++ packages/api/database/schema.ts
@@ -28,22 +28,22 @@ export const vendor = mysqlTable("vendor", {
 // Rewards Program
 export const rewards_program = mysqlTable("rewards_program", {
   program_id: serial("program_id").primaryKey(),
-  vendor_id: serial("vendor_id"), //FK to vendor
+  vendor_id: serial("vendor_id").references(() => vendor.vendor_id),
   details: text('details'),
 });
 
 // Loyalty Card
 export const loyalty_card = mysqlTable("loyalty_card", {
   loyalty_id: serial("loyalty_id").primaryKey(),
-  customer_id: serial("customer_id"), //FK to customer
-  program_id: serial("program_id"), //FK to rewards program
+  customer_id: serial("customer_id").references(() => customer.customer_id),
+  program_id: serial("program_id").references(() => rewards_program.program_id),
   points_amt: int("points_amt").notNull(),
 });
 
 // Point Redemption History
 export const point_redemption_history = mysqlTable("point_redemption_history", {
   history_id: serial("history_id").primaryKey(),
-  loyalty_id: serial("loyalty_id"), //FK to loyalty card
+  loyalty_id: serial("loyalty_id").references(() => loyalty_card.loyalty_id),
   points_redeemed: int("points_redeemed").notNull(),
   timestamp: timestamp("timestamp").notNull(),
 });
@@ -51,8 +51,8 @@ export const point_redemption_history = mysqlTable("point_redemption_history", {
 // Transaction
 export const transaction = mysqlTable("transaction", {
   transaction_id: serial("transaction_id").primaryKey(),
-  loyalty_id: serial("loyalty_id"), //FK to loyalty card
-  program_id: serial("program_id"), //FK to program
+  loyalty_id: serial("loyalty_id").references(() => loyalty_card.loyalty_id),
+  program_id: serial("program_id").references(() => rewards_program.program_id),
   purchase_amt: int("purchase_amt").notNull(),
   points_earned: int("points_earned").notNull(),
   timestamp: timestamp("timestamp").notNull(),
