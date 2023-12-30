d051fe9164eef7ba731dc5ff473945d9ce146f88
HiltonLuu <60626050+HiltonLuu@users.noreply.github.com>
2023-12-30 14:12:20 -0700

    fixed file structure and added scripts for gen and migrate

--- packages/database/migrate.ts
+++ packages/database/migrate.ts
@@ -8,7 +8,7 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { connect } from "@planetscale/database";
 import { migrate } from "drizzle-orm/planetscale-serverless/migrator";
-import * as schema from "./schema";
+//import * as schema from "./schema";
 
 // create the connection
 const connection = connect({
@@ -17,17 +17,16 @@ const connection = connect({
   password: process.env.DATABASE_PASSWORD,
 });
 
-const db = drizzle(connection, { schema });
+const db = drizzle(connection);
 
 async function main() {
   console.log("Migration started...");
-  await migrate(db, { migrationsFolder: "drizzle-migrations" });
+  await migrate(db, { migrationsFolder: "drizzle" });
   console.log("Migration ended.");
   process.exit(0);
 }
 
 main().catch((err) => {
-    console.log(err);
-    process.exit(1);
-  });
-
+  console.log(err);
+  process.exit(1);
+});
