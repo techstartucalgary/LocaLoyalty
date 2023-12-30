4c6f0c681b7152ec92c04ae4e3b01050de08922e
mpagels3022 <101290617+mpagels3022@users.noreply.github.com>
2023-12-30 14:33:09 -0700

    Removed foreign keys from schema

--- packages/database/schema.ts
+++ packages/database/schema.ts
@@ -35,22 +35,22 @@ export const vendor = mysqlTable("vendor", {
 // Rewards Program
 export const rewards_program = mysqlTable("rewards_program", {
   program_id: serial("program_id").primaryKey(),
-  vendor_id: int("vendor_id").references(() => vendor.vendor_id),
+  vendor_id: int("vendor_id"),  //references vendor.vendor_id
   details: text("details"),
 });
 
 // Loyalty Card
 export const loyalty_card = mysqlTable("loyalty_card", {
   loyalty_id: serial("loyalty_id").primaryKey(),
-  customer_id: int("customer_id").references(() => customer.customer_id),
-  program_id: int("program_id").references(() => rewards_program.program_id),
+  customer_id: int("customer_id"),  //references customer.customer_id
+  program_id: int("program_id"),  //references rewards_program.program_id
   points_amt: int("points_amt").notNull(),
 });
 
 // Point Redemption History
 export const point_redemption_history = mysqlTable("point_redemption_history", {
   history_id: serial("history_id").primaryKey(),
-  loyalty_id: int("loyalty_id").references(() => loyalty_card.loyalty_id),
+  loyalty_id: int("loyalty_id"),  //references loyalty_card.loyalty_id
   points_redeemed: int("points_redeemed").notNull(),
   timestamp: timestamp("timestamp").notNull(),
 });
@@ -58,8 +58,8 @@ export const point_redemption_history = mysqlTable("point_redemption_history", {
 // Transaction
 export const transaction = mysqlTable("transaction", {
   transaction_id: serial("transaction_id").primaryKey(),
-  loyalty_id: int("loyalty_id").references(() => loyalty_card.loyalty_id),
-  program_id: int("program_id").references(() => rewards_program.program_id),
+  loyalty_id: int("loyalty_id"), //references loyalty_card.loyalty_id
+  program_id: int("program_id"), //references rewards_program.program_id
   purchase_amt: int("purchase_amt").notNull(),
   points_earned: int("points_earned").notNull(),
   timestamp: timestamp("timestamp").notNull(),
