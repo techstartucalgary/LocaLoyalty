46b834ceb7421a16161675e7d56909da10c96253
HiltonLuu <60626050+HiltonLuu@users.noreply.github.com>
2024-01-15 22:30:45 -0700

    added zustand to website and refactored to public and auth pages and generalized api calls
    Signed-off-by: HiltonLuu <60626050+HiltonLuu@users.noreply.github.com>

---
+++ next/src/components/AuthNavbar.tsx
@@ -0,0 +1,19 @@
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 text-white shadow-lg py-5 flex flex-row sticky top-0 z-50">
+      {/* Website Logo */}
+      <Link href="/auth" className="text-xl">
+        LocaLoyalty
+      </Link>
+
+      <UserButton afterSignOutUrl="/" />
+
+      {/* Primary Navbar items */}
+    </nav>
+  );
+};
+
+export default Navbar;
