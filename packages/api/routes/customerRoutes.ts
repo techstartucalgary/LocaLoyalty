65b47143dc5a742a599b70b7a890a856f50a2db6
Bradley Li <jizhe.li@ucalgary.ca>
2024-01-29 16:07:06 -0700

    added API endpoint /vendor for displaying all of the available vendors
    Signed-off-by: Bradley Li <jizhe.li@ucalgary.ca>

--- packages/api/routes/customerRoutes.ts
+++ packages/api/routes/customerRoutes.ts
@@ -1,9 +1,18 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import * as customerController from "../controllers/customerController";
+import * as vendorController from "../controllers/vendorController";
+
 const router = express.Router();
 
 // Sample route
 router.get("/sample", (req, res) => {
   res.send({ message: "This is a sample response" });
 });
+
+// API endpoint to browse all of the available vendors
+router.get("/vendors", vendorController.index);
+
+// API endpoint to view all of the associated vendors
+// router.get("/cards", customerController.getAllCards);
 
 export default router;
