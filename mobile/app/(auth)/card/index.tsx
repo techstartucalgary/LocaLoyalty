e0f7bbf9a79cd20eb715b423dd7f65dbc139f17e
anthonyjdam <anthonydam1968@gmail.com>
2024-02-06 18:27:04 -0700

    fixed db errors and updated swagger

--- mobile/app/(auth)/card/index.tsx
+++ mobile/app/(auth)/card/index.tsx
@@ -1,5 +1,5 @@
 import { View, Text, Image, FlatList, Pressable } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { useUser } from "@clerk/clerk-expo";
 import { Link } from "expo-router";
 import { cardData } from "../../../content/temp-card-data";
@@ -119,6 +119,12 @@ const Card = ({
 };
 
 const Wallet = () => {
+
+	useEffect(() => {
+
+		
+	})
+
 	return (
 		<View className="h-full w-full bg-[#F7F8F8]">
 			<View className="w-full bg-[#9FBAFF] pb-8 h-52 rounded-b-[40px] pt-32 px-8">
