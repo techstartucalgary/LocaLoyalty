5d6ea3725219317fbc19742b24cf3c0119da5a2e
HiltonLuu <60626050+HiltonLuu@users.noreply.github.com>
2024-03-22 12:19:51 -0600

    working turso migrations tables have been made on db instance
    Signed-off-by: HiltonLuu <60626050+HiltonLuu@users.noreply.github.com>

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,11 +1,12 @@
 import "dotenv/config";
-import type {Config} from "drizzle-kit";
+import type { Config } from "drizzle-kit";
 
 export default {
   schema: "./schema.ts",
   out: "./drizzle",
-  driver: "better-sqlite",
+  driver: "turso",
   dbCredentials: {
     url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
 } satisfies Config;
--- packages/database/schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var sqlite_core_1 = require("drizzle-orm/sqlite-core");
-var foo = (0, sqlite_core_1.sqliteTable)("foo", {
-    bar: (0, sqlite_core_1.text)("bar").notNull()["default"]("Hey!")
-});
-var foo2 = (0, sqlite_core_1.sqliteTable)("foo2", {
-    bar: (0, sqlite_core_1.text)("bar").notNull()["default"]("Hey!")
-});
