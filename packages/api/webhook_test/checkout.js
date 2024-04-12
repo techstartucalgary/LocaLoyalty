ff9d04015464aebd9c3ae2401feaef954cfb6c3a
max3p <max.pagels.3022@gmail.com>
2024-04-11 18:57:52 -0600

    added auth key to checkout test

--- packages/api/webhook_test/checkout.js
+++ packages/api/webhook_test/checkout.js
@@ -7,41 +7,43 @@ bread loaf id: GBV9R3WWJHPEE
 cheese bun id: X7JAEET37MWVR
 */
 
-    const options = {
-        method: 'POST',
-        headers: {
-          accept: 'application/json',
-          'content-type': 'application/json',
-          authorization: 'Bearer {oAuth token}'
-        },
-        body: JSON.stringify({
-          orderCart: {
+import dotenv from "dotenv";
+
+const options = {
+    method: 'POST',
+    headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+        authorization: 'Bearer f7ed0fb1-710c-6b16-9942-57f37f5e3913'
+    },
+    body: JSON.stringify({
+        orderCart: {
             discounts: [],
             lineItems: [
-              {
-                item: {id: 'X7JAEET37MWVR'}, //cheese bun id    
-                printed: 'false',
-                exchanged: 'false',
-                modifications: [],
-                refunded: 'false',
-                refund: {transactionInfo: {isTokenBasedTx: 'false', emergencyFlag: 'false'}},
-                isRevenue: 'false'
-              }
+                {
+                    item: { id: 'X7JAEET37MWVR' }, //cheese bun id    
+                    printed: 'false',
+                    exchanged: 'false',
+                    modifications: [],
+                    refunded: 'false',
+                    refund: { transactionInfo: { isTokenBasedTx: 'false', emergencyFlag: 'false' } },
+                    isRevenue: 'false'
+                }
             ],
             orderType: {
-              taxable: 'false',
-              isDefault: 'false',
-              filterCategories: 'false',
-              isHidden: 'false',
-              isDeleted: 'false',
-              id: 'GAM80DMM3DEDG' //random order id?
+                taxable: 'false',
+                isDefault: 'false',
+                filterCategories: 'false',
+                isHidden: 'false',
+                isDeleted: 'false',
+                id: 'GAM80DMM3DEDG' //random order id?
             },
             groupLineItems: 'false'
-          }
-        })
-      };
-      
-      fetch('https://sandbox.dev.clover.com/v3/merchants/8830RN7Z4YNE1/atomic_order/orders', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+        }
+    })
+};
+
+fetch('https://sandbox.dev.clover.com/v3/merchants/8830RN7Z4YNE1/atomic_order/orders', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
